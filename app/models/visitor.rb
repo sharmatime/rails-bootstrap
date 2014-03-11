4d49dcf0c51567ef8cad21b40840d31b2690f60e
Adrian Sharma <sharma.adrian@gmail.com>
2014-03-11 18:57:36 -0400

    refine home page styling

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -6,6 +6,21 @@ class Visitor < ActiveRecord::Base
 
 	#Create a constant, which returns an array containing a name for each image the visitor will see
 	#we'll use this contstant in multiple places, so by creating it in the model, it means its hardcoded only once
-	IMAGE_LABELS = ['Salty', 'Simba King', 'Thomas Jefferson', 'Ranger'] 
+	IMAGE_LABELS = ['Salty', 'King', 'Thomas Jefferson', 'Ranger', 'Coco'] 
+
+	def update_spreadsheet
+		connection = GoogleDrive.login(Rails.application.secrets.gmail_username, Rails.application.secrets.gmail_password)
+		ss = connection.spreadsheet_by_title('Rails-Bootstrap-Example')
+		if ss.nil?
+			ss = connection.create_spreadsheet('Rails-Bootstrap-Example')
+		end
+		ws = ss.worksheets[0]
+		last_row = 1 + ws.num_rows
+		ws[last_row, 1] = Time.now
+		ws[last_row, 2] = self.favorite
+		ws[last_row, 3] = self.comment
+		ws.save
+	end
 
 end
+
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -142,4 +142,5 @@ SimpleForm.setup do |config|
 
   # Default class for inputs
   # config.input_class = nil
+  config.default_input_size = 100
 end
